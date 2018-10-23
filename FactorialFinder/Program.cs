bdf6a2a7cb05518e6d3f74ab31b61fe2109fc957
GeruN <33115757+GerunNik@users.noreply.github.com>
2018-10-23 16:25:12 +0200

    Add files via upload

---
+++ FactorialFinder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FactorialFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number to find the Factorial of it");
+            long factorialNumber = Convert.ToInt32(Console.ReadLine());
+
+            long factorialCalculated = factorial(factorialNumber + 1) / (factorialNumber + 1);
+
+            Console.WriteLine("The factorial is: " + factorialCalculated); // 39! equals int Overflow
+            Console.ReadKey();
+        }
+        static long factorial(long value)
+        {
+            long factorialCalculated;
+
+            if (value != 0)
+            {
+                factorialCalculated = 1;
+                for (int i = 0; i < value; i++)
+                {
+                    factorialCalculated = (value - i) * factorialCalculated;
+                }
+            }
+            else
+            {
+                factorialCalculated = 1;
+            }
+
+            return factorialCalculated;
+        }
+    }
+}
